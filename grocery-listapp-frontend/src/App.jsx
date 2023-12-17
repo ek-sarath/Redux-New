b53dc4a19253dd4059c3144689a3aea5d7a66c58
ek-sarath <sarath.e@edstem.com>
2023-12-17 20:36:10 +0530

    First Commit

---
+++ grocery-listapp-frontend/src/App.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { store } from '../src/redux/store';
+import { Login } from './components/Login';
+import { Register } from './components/Register';
+import GroceryListApp from './components/GroceryListApp';
+
+const queryClient = new QueryClient();
+
+function App() {
+  const [currentForm, setCurrentForm] = useState('login');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  };
+
+  const handleLoginSuccess = () => {
+    setIsLoggedIn(true);
+  };
+
+  return (
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          {isLoggedIn ? (
+            <GroceryListApp />
+          ) : (
+            currentForm === 'login' ? (
+              <Login onFormSwitch={toggleForm} onLoginSuccess={handleLoginSuccess} />
+            ) : (
+              <Register onFormSwitch={toggleForm} />
+            )
+          )}
+        </div>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </Provider>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import '../src/App.css';
+// import {Login} from "./components/Login";
+// import {Register} from "./components/Register";
+// import GroceryListApp from "./components/GroceryListApp";
+
+// function App() {
+//   const [currentForm, setCurrentForm] = useState('login');
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//   const toggleForm = (formName) => {
+//     setCurrentForm(formName);
+//   }
+
+//   const handleLoginSuccess = () => {
+//     setIsLoggedIn(true);
+//   }
+
+//   return (
+//     <div className="App">
+//       {isLoggedIn ? (
+//         <GroceryListApp />
+//       ) : (
+//         currentForm === "login" ? (
+//           <Login onFormSwitch={toggleForm} onLoginSuccess={handleLoginSuccess} />
+//         ) : (
+//           <Register onFormSwitch={toggleForm} />
+//         )
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ grocery-listapp-frontend/src/components/Login.jsx
@@ -0,0 +1,140 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useLoginMutation } from '../redux/store';
+
+export const Login = (props) => {
+  const dispatch = useDispatch();
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await dispatch(
+        useLoginMutation({ email, password })
+      );
+      props.onLoginSuccess();
+    } catch (error) {
+      console.error('Login failed', error);
+      props.setErrorMessage('Invalid username or password. Try Again');
+    }
+  };
+
+  return (
+        <div className="auth-form-container">
+        <h5>List Manager</h5>
+          <form className="login-form" onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              type="email"
+              placeholder="youremail@gmail.com"
+              id="email"
+              name="email"
+              required
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              type="password"
+              placeholder="********"
+              id="password"
+              name="password"
+              required
+            />
+            <button type="submit" className="LogInButton">Log In</button>
+          </form>
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+          <button
+            className="link-btn"
+            onClick={() => props.onFormSwitch('register')}
+          >
+            Don't have an account? Register here.
+          </button>
+        </div>
+    
+      );
+    };
+
+
+
+
+
+
+
+
+
+
+// import React, { useState } from "react";
+// import axios from "axios";
+
+// export const Login = (props) => {
+//   const [email, setEmail] = useState('');
+//   const [password, setPassword] = useState('');
+//   const [errorMessage, setErrorMessage] = useState('');
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+
+//     try {
+//       if (!email || !password) {
+//         setErrorMessage("Email and password are required.");
+//         return;
+//       }
+
+//       const response = await axios.post("http://localhost:8085/user/login", {
+//         email,
+//         password,
+//       });
+
+//       console.log("Login successful", response.data);
+//       const loginSuccessful = true;
+//       if (loginSuccessful) {
+//         props.onLoginSuccess();
+//       }
+//     } catch (error) {
+//       console.error("Login failed", error);
+//       setErrorMessage("Invalid username or password. Try Again");
+//     }
+//   };
+
+//   return (
+//     <div className="auth-form-container">
+//     <h5>List Manager</h5>
+//       <form className="login-form" onSubmit={handleSubmit}>
+//         <label htmlFor="email">Email</label>
+//         <input
+//           value={email}
+//           onChange={(e) => setEmail(e.target.value)}
+//           type="email"
+//           placeholder="youremail@gmail.com"
+//           id="email"
+//           name="email"
+//           required
+//         />
+//         <label htmlFor="password">Password</label>
+//         <input
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//           type="password"
+//           placeholder="********"
+//           id="password"
+//           name="password"
+//           required
+//         />
+//         <button type="submit" className="LogInButton">Log In</button>
+//       </form>
+//       {errorMessage && <p className="error-message">{errorMessage}</p>}
+//       <button
+//         className="link-btn"
+//         onClick={() => props.onFormSwitch('register')}
+//       >
+//         Don't have an account? Register here.
+//       </button>
+//     </div>
+
+//   );
+// };
